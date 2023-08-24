50c6d40a4616e0bee3234238534302f6c7343d1b
dienifergraziela <dienifergrazielaripl@gmail.com>
2023-08-23 22:06:49 -0300

    projeto finalizado

--- app.js
+++ app.js
@@ -7,28 +7,11 @@ angular.module('blog').controller('Rest', function ($scope, $http) {
     );
 });
 
-angular.module('blogMax', []);
-angular.module('blogMax').controller('RestMax', function ($scope, $http) {
-  $http.get('https://api-rest-post-diegocandido.herokuapp.com/postagem/2').
+angular.module('blog2', []);
+angular.module('blog2').controller('Rest2', function ($scope, $http) {
+  $http.get('https://api-rest-post-diegocandido.herokuapp.com/postagem/0').
     success(function (data) {
       $scope.publicacoes = data;
     }
     );
 });
-/*
-angular.module('blogMax', []);
-angular.module('blogMax').controller('RestMax', function ($scope, $http, $location){
-  var absUrl = $location.absUrl();
-  var url = absUrl.split("#/");
-  console.log(url);
-  console.log($location);
-  $http({
-    method: 'GET',
-    url: 'https://api-rest-post-diegocandido.herokuapp.com/postagem/'+postId
-  }).then(function(data){
-    $scope.publicacoes = data.data;
-    console.log(data.data);
-  },function (error){
-
-  });
-})*/
